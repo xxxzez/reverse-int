06c58df4ef1616a992752481f22b91412b72ab56
Matvei Bohush <bohush.mathew@gmail.com>
2020-02-19 19:52:24 +0300

    refactoring

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
 module.exports = function reverse (n) {
-  let x = Math.abs(n).toString();
-  let z = x.split('');
-  let zz = z.reverse();
-  let c = zz.join('');
-  return Number(c);
+  return Number(Math.abs(n).toString().split('').reverse().join(''));
 }
